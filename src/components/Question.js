7ebdc724119cc49911829c9fd162980858a58215
Abram Shook <ajshook77@gmail.com>
2022-05-23 07:56:18 -0500

    commit tree 8fd23fd6aa0b62913c25c41e40a219518f34e75c

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,32 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react'
 
 function Question({ question, onAnswered }) {
-  const [timeRemaining, setTimeRemaining] = useState(10);
+  const [timeRemaining, setTimeRemaining] = useState(10)
 
-  // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (timeRemaining >= 1) setTimeRemaining(timeRemaining - 1)
+      else if (timeRemaining === 0) {
+        setTimeRemaining(10)
+        onAnswered(false)
+      }
+    }, 1000)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [timeRemaining, onAnswered])
 
   function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
+    setTimeRemaining(10)
+    onAnswered(isCorrect)
   }
 
-  const { id, prompt, answers, correctIndex } = question;
+  const { id, prompt, answers, correctIndex } = question
 
   return (
     <>
       <h1>Question {id}</h1>
       <h3>{prompt}</h3>
       {answers.map((answer, index) => {
-        const isCorrect = index === correctIndex;
+        const isCorrect = index === correctIndex
         return (
           <button key={answer} onClick={() => handleAnswer(isCorrect)}>
             {answer}
           </button>
-        );
+        )
       })}
       <h5>{timeRemaining} seconds remaining</h5>
     </>
-  );
+  )
 }
 
-export default Question;
+export default Question
